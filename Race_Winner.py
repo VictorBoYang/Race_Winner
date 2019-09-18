f6289a1472a4646f4db936e454b9314d1c5cae89
VictorBoYang <VictorBoYang@gmail.com>
2019-09-18 19:30:30 -0400

    now user can decide amount of players

--- Race_Winner.py
+++ Race_Winner.py
@@ -1,6 +1,21 @@
 from function_storage import *
 
-player_amount = 4
+# def decide_playerAmount(player_amount):
+#     amount = input("Please enter the amount of players: ")
+#     player_amount = int(amount)
+#     while player_amount <= 2:
+#         print("Invalid amount entry. Please enter amount >= 3. Try again.")
+#         amount = input("Please enter the amount of players: ")
+#         player_amount = int(amount)
+# # player_amount = 0
+# decide_playerAmount(player_amount)
+amount = input("Please enter the amount of players: ")
+player_amount = int(amount)
+while player_amount <= 2:
+    print("Invalid amount entry. Please enter amount >= 3. Try again.")
+    amount = input("Please enter the amount of players: ")
+    player_amount = int(amount)
+
 player_array = [Player("",0)] * player_amount
 
 user_input(player_amount,player_array)
